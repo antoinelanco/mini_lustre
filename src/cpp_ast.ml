67f08d4368a08d90526ae30f991e392cf4dae7eb
antoinelanco <al@pmspos.com>
2019-03-02 17:35:08 +0100

    compil

--- src/cpp_ast.ml
+++ src/cpp_ast.ml
@@ -4,12 +4,8 @@ open Asttypes
 
 type typed_var = string * base_ty
 
-type cpp_expr =
-  { cppexpr_desc: cpp_expr_desc
-  ; cppexpr_type: ty
-  }
 
-and cpp_expr_desc =
+and cpp_expr =
   | CPP_const  of const
   | CPP_ident  of string
   | CPP_op     of op * cpp_expr list
@@ -17,17 +13,11 @@ and cpp_expr_desc =
   | CPP_prim   of string * cpp_expr list
   | CPP_arrow  of cpp_expr * cpp_expr
   | CPP_pre    of cpp_expr
-  | CPP_tuple  of cpp_expr list
 
-(* Les patterns sont exprimés grâce à std::tie *)
-type cpp_patt =
-  { cpppatt_desc: string list
-  ; cpppatt_type: ty
-  }
 
 type cpp_affect =
-  { cppeq_patt: cpp_patt
-  ; cppeq_expr: cpp_expr
+  { cppeq_patt: string list
+  ; cppeq_expr: cpp_expr list
   }
 
 (* Les fonctions lustre sont représentées par des lambda fonctions,
